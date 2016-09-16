6ba810c75ad75cfecf5e72b756c1bceb2af1c93f
Kijin Sung <kijin@kijinsung.com>
2016-09-16 20:11:05 +0900

    Prevent caching other pages unless the user understands what he's doing

--- tpl/js/config.js
+++ tpl/js/config.js
@@ -3,6 +3,11 @@ jQuery(function() {
 	
 	var $ = jQuery;
 	
+	$("a.sc_enable_other_pages").on("click", function(event) {
+		event.preventDefault();
+		$("#sc_full_cache_type_other").removeAttr("disabled");
+	});
+	
 	$("span.console_font").on("click", function() {
 		$("#sc_core_object_cache").val($(this).text());
 	});
