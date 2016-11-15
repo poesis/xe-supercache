e64704c24892cc8139ce239aab9d0dc4b571fa34
Kijin Sung <kijin@kijinsung.com>
2016-11-15 12:08:51 +0900

    Highlight cautions and warnings

--- tpl/js/config.js
+++ tpl/js/config.js
@@ -12,4 +12,10 @@ jQuery(function() {
 		$("#sc_core_object_cache").val($(this).text());
 	});
 	
+	$("p.x_help-block").each(function() {
+		var content = $.trim($(this).html());
+		content = content.replace(/(경고|Warning): /, '<span class="warning_block">$1</span> ');
+		content = content.replace(/(주의|Caution): /, '<span class="caution_block">$1</span> ');
+		$(this).html(content);
+	});
 });
